b87f9438132d54aaec84e1e5a1b7200c6c71f5a7
Matt Styles <matt@personalurban.com>
2018-09-05 22:13:10 +0100

    add animatron module

--- packages/app/src/components/app.jsx
+++ packages/app/src/components/app.jsx
@@ -1,8 +1,16 @@
 
+import React, { Fragment } from 'react'
 import { ThemeProvider } from 'styled-components'
+import { AppearRight } from 'animatron'
 
 export const App = ({ state }) => (
   <ThemeProvider theme={{}}>
-    <h1>Odyssey:Rogue One</h1>
+    <Fragment>
+      <h1>Odyssey:Rogue One</h1>
+      <AppearRight in appear delay={{
+        enter: 500,
+        exit: 0
+      }}><p>This uses a component from <code>animatron</code></p></AppearRight>
+    </Fragment>
   </ThemeProvider>
 )
